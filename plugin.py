9b4fab05b4efcaa9ddf7f13ad97b7dfeb88394a8
spline <reticulatingspline@github.com>
2014-09-20 07:14:25 -0400

    Screwed up the last fix. Should work now.

--- plugin.py
+++ plugin.py
@@ -301,11 +301,8 @@ class SoccerLive(callbacks.Plugin):
             elif dtstring.endswith("ET"):
                 naive = datetime.datetime.strptime(dtstring, "%b %d %Y %I:%M %p ET")  
                 local = pytz.timezone("US/Eastern")
-            elif dtstring.endswith("PM"):
-                naive = datetime.datetime.strptime(dtstring, "%b %d %Y %I:%M PM")
-                local = pytz.timezone("UTC")
-            elif dtstring.endswith("AM"):
-                naive = datetime.datetime.strptime(dtstring, "%b %d %Y %I:%M AM")
+            elif dtstring.endswith("AM") or dtstring.endswith("PM"):
+                naive = datetime.datetime.strptime(dtstring, "%b %d %Y %I:%M %p")
                 local = pytz.timezone("UTC")
             else:  # can't figure it out.
                 self.log.info("ERROR: Trying to parse {0} into GMT".format(dtstring))
