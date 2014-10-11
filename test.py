aede9986b0d55e1cf735caeebc52e6df6c658975
spline <reticulatingspline@github.com>
2014-10-11 14:35:20 -0400

    Starting tests.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2013, spline
+# Copyright (c) 2013-2014, spline
 # All rights reserved.
 #
 #
@@ -10,5 +10,9 @@ from supybot.test import *
 class SoccerLiveTestCase(PluginTestCase):
     plugins = ('SoccerLive',)
 
+    def testSoccerLive(self):
+        self.assertSnarfResponse('join #test', 'The operation succeeded.')
+
+    
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
